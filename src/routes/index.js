e455e5719fa6ffb9f80812328cbb291620315157
llanito7361 <llanito.code.log@gmail.com>
2024-11-09 13:33:54 -0300

    endopoints news

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,6 +13,15 @@ router.get('/lari',(req,res)=>{
     res.status(200).send('tkm bebu <3 te hago estas lineas, desde honolulu')
 })
 
+router.get('/papucho',(req,res)=>{
+
+    res.status(200).send('te amo mucho papucho, grax x todo<3')
+})
+router.get('/mamushka',(req,res)=>{
+
+    res.status(200).send('te amo mucho mamushka, grax x la torta <3')
+})
+
 
 router.use('/products',ProductRouter)
 
